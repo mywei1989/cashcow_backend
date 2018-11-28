d32a6a7534d2ff4fb3a6f7ee97930e6edb9760c8
zhiwei.lin <zhiwei.lin@mobvista.com>
2018-11-28 18:02:03 +0800

    init

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -56,7 +56,7 @@ class WechatController extends Controller {
     const { ctx, app, logger } = this;
 
     if (!wechatHandle) {
-      app.logger.info('wechatHandle is null new it');
+      logger.info('wechatHandle is null new it');
       wechatHandle = wechat(app.config.wechat).middleware(async (message, ctx) => {
         // TODO
         /**
@@ -67,14 +67,15 @@ class WechatController extends Controller {
          * Content: '6',
          * MsgId: '6494513464291382253' }
          */
-        app.logger.info('message = %o', message);
+        logger.info('message = %o', message);
         /**
          * 相应请求并且不回复内容场景下进行如下二选一回复即可
          * 1、直接回复success（推荐方式）
          * 2、直接回复空串（指字节长度为0的空字符串，而不是XML结构体中content字段的内容为空）
          * 参考：https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140543
          */
-        return 'success';
+        //return 'success';
+        return JSON.stringify(message);
       });
     }
     logger.info('after new instance');
--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = appInfo => {
   const config = exports = {};
 
@@ -8,6 +10,50 @@ module.exports = appInfo => {
 
   // add your config here
   config.middleware = [];
+
+  config.security = {
+    csrf: {
+      ignore: '/wechat', // 忽略微信推送 POST 消息的 csrf 验证
+    },
+  };
+
+  // 请在 config.prod.js 中配置实际的值
+  config.wechat = {
+    token: '',
+    appid: '',
+    encodingAESKey: '',
+    secret: '',
+  };
+
+  config.wechatApi = {
+    appId: '',
+    appSecret: '',
+  };
+
+  config.redis = {
+    client: {
+      port: 6379, // Redis port
+      host: '127.0.0.1', // Redis host
+      password: 'null',
+      db: 0,
+    },
+  };
+
+  // view ejs engine config
+  config.view = {
+    mapping: {
+      '.ejs': 'ejs',
+    },
+  };
+
+  config.ejs = {
+    root: path.join(appInfo.baseDir, 'app/view'),
+    cache: true,
+    debug: false,
+    compileDebug: true,
+    delimiter: null,
+    strict: false,
+  };
 
   return config;
 };
