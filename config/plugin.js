6cc458204c186d830ab6b03e294ae1986f17d751
zhiwei.lin <zhiwei.lin@mobvista.com>
2018-11-30 10:17:21 +0800

    no message

--- config/plugin.js
+++ config/plugin.js
@@ -3,15 +3,15 @@
 // had enabled by egg
 // exports.static = true;
 
-// exports.wechatApi = {
-//   enable: true,
-//   package: 'egg-wechat-api',
-// };
+exports.wechatApi = {
+  enable: true,
+  package: 'egg-wechat-api',
+};
 
-// exports.redis = {
-//   enable: true,
-//   package: 'egg-redis',
-// };
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+};
 exports.ejs = {
   enable: true,
   package: 'egg-view-ejs',
