291eb2c0dc5dd1718ef35783e402774eb44ad8b8
zhiwei.lin <zhiwei.lin@mobvista.com>
2018-12-05 10:43:07 +0800

    no message

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const OAuth = require('co-wechat-oauth');
 
 module.exports = app => {
   app.beforeStart(async () => {
-    try{
-      app.OAuth = new OAuth(app.config.wechat.appid, app.config.wechat.secret);
-    }catch(e){
-      app.logger.error('new OAuth error', e);
-    }
+    // try{
+    //   app.OAuth = new OAuth(app.config.wechat.appid, app.config.wechat.secret);
+    // }catch(e){
+    //   app.logger.error('new OAuth error', e);
+    // }
     const ctx = app.createAnonymousContext();
     let result = await ctx.service.wechat.menu.createMenu();
   });
